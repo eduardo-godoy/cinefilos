fc1605be376209e0564299680fc67c53b4d83df6
Eduardo Godoy <eduardogodoy.dw@gmail.com>
2024-07-14 11:35:43 -0300

    Instalando todas las dependencias necesarias + armado de la base de datos json para crear la base de datos

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, SafeAreaView, StatusBar, Platform, Text } from "react-native";
+import { useFonts } from "expo-font";
+import { colors } from "./src/global/colors";
+
+import Navigator from "./src/navigation/Navigator";
+
+import { Provider } from "react-redux";
+import store from "./src/store";
+
+import { initSQLiteDB } from "./src/persistence"; 
+
+(async ()=> {
+  try {
+    const response = await initSQLiteDB()
+    //console.log(response)
+    console.log({responseCreatingDB: response})
+    console.log("DB Inicilized")
+  } catch (error) {
+    console.log({errorCreatingDB: error})
+  }
+})()
+
 
 export default function App() {
+
+  const [fontsLoaded, fontError] = useFonts({
+    Josefin: require("./assets/JosefinSans-Regular.ttf"),
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Provider store={store}>
+        <Navigator />
+      </Provider>
+    </SafeAreaView>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.lightGray,
   },
 });
